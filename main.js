9b24df368da0d1651e6fb57053eba7c1daf971ed
Jaqueline Nunes-Heissenberger <anunes.jaqueline@gmail.com>
2023-12-27 22:13:31 +0100

    Add function acceptData

---
+++ main.js
@@ -0,0 +1,47 @@
+let form = document.getElementById('form');
+let textInput = document.getElementById('textInput');
+let msg = document.getElementById('msg');
+let dateInput = document.getElementById('dateInput');
+let textarea = document.getElementById('textarea');
+
+let tasks = document.getElementById('tasks');
+let add = document.getElementById('add');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    formValidation();
+})
+
+let data = [];
+
+let acceptData = ()=> {
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textarea.value,
+    });
+localStorage.setItem("data", JSON.stringify(data));
+
+console.log(data);
+};
+
+let formValidation = ()=>{
+    if(textInput.value === ""){
+        console.log('failure');
+        msg.innerHTML = "Task cannot be blank";
+        
+    }
+    else{
+        console.log('success');
+        msg.innerHTML = "";
+        
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+            add.setAttribute("data-bs-dismiss", "");
+        })();
+    }
+};
+
